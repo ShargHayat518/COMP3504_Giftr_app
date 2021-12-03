fa3d02eab20a0e3ad6beca288b7338c34c4ed176
Shargeel Hayat <sharg1930@gmail.com>
2021-12-02 19:47:08 -0700

    added some amplify stuff

--- amplify/backend/function/function/src/app.js
+++ amplify/backend/function/function/src/app.js
@@ -32,7 +32,8 @@ app.use(function(req, res, next) {
 
 app.get('/items', function(req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  Amplify.Storage.
+  res.json({success: 'get call succeed! YAY', url: req.url});
 });
 
 app.get('/items/*', function(req, res) {
